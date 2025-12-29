505b8ab2b849f3e27c38c7d382c25c872e26c88a
Rickard Holmberg <rickard@holmberg.info>
2025-12-29 18:00:44 +0100

    Security fixes and CLAUDE.md (#5)
    * Missed macos target updates
    
    * Security fixes
    
    * Add CLAUDE.md
    
    * Remove test rendering functionality from settings dialog
    
    * Update CLAUDE.md

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "TeXClipper",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v14)
     ],
     products: [
         .executable(
